40721f31a5282ed7da8fde236a4f4e6fd531d091
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-08 13:41:28 +0000

    add method into classes

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-console.log(typeof '')
-
-const user={
-    name:'Andrii',
-    getName:()=>{
-        
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    getName() {
+        return `${this.name} has ${this.age}`
     }
 }
+
+const Person1 = new Person("Anddrii", 37)
+const Person2 = new Person("Yana", 32)
+console.log(Person1.getName())
+console.log(Person2.getName())
+
+class House {
+    constructor(adress, price, residents) {
+        this.adress = adress
+        this.price = price
+        this.residents = residents
+    }
+    getAdress() {
+        return this.adress
+    }
+    getPrice() {
+        return this.price
+    }
+    getResidents() {
+        return this.residents
+    }
+    addresident(resident){
+        this.residents.push(resident)
+
+    }
+}
+
+let Pedro=new Person('Pedro',19)
+let David = new Person('David',21)
+let house=new House('Premogi',280000,[Pedro,David])
+console.log(house.getResidents())
+
+let Paulo=new Person('Paulo',18)
+house.addresident(Paulo)
+console.log(house.getResidents())
